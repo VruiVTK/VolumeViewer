9bab8a70fd8b04b006551eb4f7c8fcd9d8dc24eb
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-01-09 16:29:53 -0500

    Make compatibility with new ExternalVTKWidget API

--- DataItem.cpp
+++ DataItem.cpp
@@ -25,24 +25,25 @@ ExampleVTKReader::DataItem::DataItem(void)
   this->externalVTKWidget = vtkSmartPointer<ExternalVTKWidget>::New();
 
   /* Use depth peeling to enable transparency */
-  this->externalVTKWidget->GetRenderer()->SetUseDepthPeeling(1);
-  this->externalVTKWidget->GetRenderer()->SetMaximumNumberOfPeels(4);
-  this->externalVTKWidget->GetRenderer()->SetOcclusionRatio(0.1);
+  vtkRenderer* renderer = this->externalVTKWidget->AddRenderer();
+  renderer->SetUseDepthPeeling(1);
+  renderer->SetMaximumNumberOfPeels(4);
+  renderer->SetOcclusionRatio(0.1);
 
   this->actor = vtkSmartPointer<vtkActor>::New();
   this->lowActor = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actor);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActor);
+  renderer->AddActor(this->actor);
+  renderer->AddActor(this->lowActor);
 
   this->actorOutline = vtkSmartPointer<vtkActor>::New();
   this->lowActorOutline = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actorOutline);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActorOutline);
+  renderer->AddActor(this->actorOutline);
+  renderer->AddActor(this->lowActorOutline);
 
   this->actorVolume = vtkSmartPointer<vtkVolume>::New();
   this->lowActorVolume = vtkSmartPointer<vtkVolume>::New();
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->actorVolume);
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->lowActorVolume);
+  renderer->AddVolume(this->actorVolume);
+  renderer->AddVolume(this->lowActorVolume);
   this->propertyVolume = vtkSmartPointer<vtkVolumeProperty>::New();
   this->lowPropertyVolume = vtkSmartPointer<vtkVolumeProperty>::New();
 
@@ -52,31 +53,31 @@ ExampleVTKReader::DataItem::DataItem(void)
   this->lowXCutter = vtkSmartPointer<vtkCutter>::New();
   this->lowXCutterMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->lowActorXCutter = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actorXCutter);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActorXCutter);
+  renderer->AddActor(this->actorXCutter);
+  renderer->AddActor(this->lowActorXCutter);
   this->yCutter = vtkSmartPointer<vtkCutter>::New();
   this->yCutterMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->actorYCutter = vtkSmartPointer<vtkActor>::New();
   this->lowYCutter = vtkSmartPointer<vtkCutter>::New();
   this->lowYCutterMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->lowActorYCutter = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actorYCutter);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActorYCutter);
+  renderer->AddActor(this->actorYCutter);
+  renderer->AddActor(this->lowActorYCutter);
   this->zCutter = vtkSmartPointer<vtkCutter>::New();
   this->zCutterMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->actorZCutter = vtkSmartPointer<vtkActor>::New();
   this->lowZCutter = vtkSmartPointer<vtkCutter>::New();
   this->lowZCutterMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->lowActorZCutter = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actorZCutter);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActorZCutter);
+  renderer->AddActor(this->actorZCutter);
+  renderer->AddActor(this->lowActorZCutter);
 
   this->contourFilter = vtkSmartPointer<vtkContourFilter>::New();
   this->contourActor = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddActor(this->contourActor);
+  renderer->AddActor(this->contourActor);
   this->lowContourFilter = vtkSmartPointer<vtkContourFilter>::New();
   this->lowContourActor = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowContourActor);
+  renderer->AddActor(this->lowContourActor);
 
   this->actorXContourCutter = vtkSmartPointer<vtkActor>::New();
   this->actorXContourCutter->GetProperty()->SetColor(1.0, 0.0, 0.0);
@@ -84,24 +85,24 @@ ExampleVTKReader::DataItem::DataItem(void)
   this->lowActorXContourCutter = vtkSmartPointer<vtkActor>::New();
   this->lowActorXContourCutter->GetProperty()->SetColor(1.0, 0.0, 0.0);
   this->lowActorXContourCutter->GetProperty()->SetLineWidth(3);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actorXContourCutter);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActorXContourCutter);
+  renderer->AddActor(this->actorXContourCutter);
+  renderer->AddActor(this->lowActorXContourCutter);
   this->actorYContourCutter = vtkSmartPointer<vtkActor>::New();
   this->actorYContourCutter->GetProperty()->SetColor(0.0, 1.0, 0.0);
   this->actorYContourCutter->GetProperty()->SetLineWidth(3);
   this->lowActorYContourCutter = vtkSmartPointer<vtkActor>::New();
   this->lowActorYContourCutter->GetProperty()->SetColor(0.0, 1.0, 0.0);
   this->lowActorYContourCutter->GetProperty()->SetLineWidth(3);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actorYContourCutter);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActorYContourCutter);
+  renderer->AddActor(this->actorYContourCutter);
+  renderer->AddActor(this->lowActorYContourCutter);
   this->actorZContourCutter = vtkSmartPointer<vtkActor>::New();
   this->actorZContourCutter->GetProperty()->SetColor(0.0, 0.0, 1.0);
   this->actorZContourCutter->GetProperty()->SetLineWidth(3);
   this->lowActorZContourCutter = vtkSmartPointer<vtkActor>::New();
   this->lowActorZContourCutter->GetProperty()->SetColor(0.0, 0.0, 1.0);
   this->lowActorZContourCutter->GetProperty()->SetLineWidth(3);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->actorZContourCutter);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowActorZContourCutter);
+  renderer->AddActor(this->actorZContourCutter);
+  renderer->AddActor(this->lowActorZContourCutter);
 
   this->aContour = vtkSmartPointer<vtkContourFilter>::New();
   this->aContourMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
@@ -109,40 +110,40 @@ ExampleVTKReader::DataItem::DataItem(void)
   this->lowAContour = vtkSmartPointer<vtkContourFilter>::New();
   this->lowAContourMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->lowActorAContour = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->actorAContour);
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->lowActorAContour);
+  renderer->AddVolume(this->actorAContour);
+  renderer->AddVolume(this->lowActorAContour);
   this->bContour = vtkSmartPointer<vtkContourFilter>::New();
   this->bContourMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->actorBContour = vtkSmartPointer<vtkActor>::New();
   this->lowBContour = vtkSmartPointer<vtkContourFilter>::New();
   this->lowBContourMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->lowActorBContour = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->actorBContour);
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->lowActorBContour);
+  renderer->AddVolume(this->actorBContour);
+  renderer->AddVolume(this->lowActorBContour);
   this->cContour = vtkSmartPointer<vtkContourFilter>::New();
   this->cContourMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->actorCContour = vtkSmartPointer<vtkActor>::New();
   this->lowCContour = vtkSmartPointer<vtkContourFilter>::New();
   this->lowCContourMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->lowActorCContour = vtkSmartPointer<vtkActor>::New();
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->actorCContour);
-  this->externalVTKWidget->GetRenderer()->AddVolume(this->lowActorCContour);
+  renderer->AddVolume(this->actorCContour);
+  renderer->AddVolume(this->lowActorCContour);
 
   this->freeSliceCutter = vtkSmartPointer<vtkCutter>::New();
   this->freeSliceMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->freeSliceMapper->SetInputConnection(this->freeSliceCutter->GetOutputPort());
   this->freeSliceActor = vtkSmartPointer<vtkActor>::New();
   this->freeSliceActor->SetMapper(this->freeSliceMapper);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->freeSliceActor);
+  renderer->AddActor(this->freeSliceActor);
   this->lowFreeSliceCutter = vtkSmartPointer<vtkCutter>::New();
   this->lowFreeSliceMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   this->lowFreeSliceMapper->SetInputConnection(this->lowFreeSliceCutter->GetOutputPort());
   this->lowFreeSliceActor = vtkSmartPointer<vtkActor>::New();
   this->lowFreeSliceActor->SetMapper(this->lowFreeSliceMapper);
-  this->externalVTKWidget->GetRenderer()->AddActor(this->lowFreeSliceActor);
+  renderer->AddActor(this->lowFreeSliceActor);
 
   this->flashlight = vtkSmartPointer<vtkLight>::New();
-  this->externalVTKWidget->GetRenderer()->AddLight(this->flashlight);
+  renderer->AddLight(this->flashlight);
 
 
   this->colorFunction = vtkSmartPointer<vtkColorTransferFunction>::New();
